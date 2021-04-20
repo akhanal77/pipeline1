549d5a71ef7bce69152a5b0a63940b14e73a792a
Anil Khanal <khanalaneel@gmail.com>
2021-04-20 03:16:57 +0000

    Merged after stashing

--- app.sh
+++ app.sh
@@ -2,4 +2,9 @@
 echo  'Hello Pipeline'
 echo  'Learning to write stash'
 echo 'Another change'
+<<<<<<< Updated upstream
 echo 'Second change made from github'
+=======
+echo 'Yet another change'
+
+>>>>>>> Stashed changes
